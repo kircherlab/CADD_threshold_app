b5a8681eb136c628530f7d288b61a0d8e0d79fd9
coraleif <cora-leifheit@web.de>
2025-12-10 11:03:04 +0100

    test

--- data_loader.py
+++ data_loader.py
@@ -4,9 +4,11 @@ from functools import lru_cache
 
 DATA_PATH = Path(__file__).parent / "data"
 
+
 @lru_cache(maxsize=None)
 def load_metrics(version):
     return pd.read_csv(DATA_PATH / f"basic_{version}_ClinicalSignificance_PHRED_pathogenic_1_101_metrics.csv.gz", low_memory=False)
+
 
 @lru_cache(maxsize=None)
 def load_metrics_bar(version):
